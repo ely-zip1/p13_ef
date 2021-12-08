752ab2de894f51ffece9c17101a99c34745e2137
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2021-12-08 14:02:38 +0800

    Update Your_deposits.php

--- application/controllers/Your_deposits.php
+++ application/controllers/Your_deposits.php
@@ -26,9 +26,9 @@ class Your_deposits extends CI_Controller
     $data['date_registered'] = $this->session->userdata('date_registered');
 
     $account_balance = $this->Account_model->get_account_balance($this->session->user_id);
-    
+
     $data['account_balance'] = number_format($account_balance, 2, '.', ',');
-        
+
     $member = $this->Members->get_member($this->session->username);
     $deposits = $this->DepositModel->get_deposit_per_member($member->id);
 
@@ -37,136 +37,133 @@ class Your_deposits extends CI_Controller
     $plan2_deposit_data = array();
     $plan3_deposit_data = array();
     $plan4_deposit_data = array();
-    
-    $plan1_days = 200;
-    $plan2_days = 180;
-    $plan3_days = 150;
+
+    $plan1_days = 180;
+    $plan2_days = 150;
+    $plan3_days = 120;
 
     foreach ($deposits as $deposit) {
-      if($deposit->is_pending == 0){
+      if ($deposit->is_pending == 0) {
         $total_deposit += $deposit->amount;
       }
 
-        if($deposit->package_id == 1){
-          $plan1 = array();
-          $plan1['amount'] = number_format($deposit->amount, 2, '.', ',');
+      if ($deposit->package_id == 1) {
+        $plan1 = array();
+        $plan1['amount'] = number_format($deposit->amount, 2, '.', ',');
 
-          $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
-          $plan1['mode'] = $payment_mode->name;
+        $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
+        $plan1['mode'] = $payment_mode->name;
 
-          if($payment_mode->name == 'XRP'){
-            $plan1['send_to'] = $payment_mode->tag .' / '. $payment_mode->account;
-          }else{
-            $plan1['send_to'] = $payment_mode->account;
-          }
-
-          $plan1['date'] = $deposit->date;
-          $plan1['date_approved'] = $deposit->date_approved;
-
-          if($deposit->deposit_options_id == 7){
-            $plan1['status'] = 'Processed';
-          }else{
-            $plan1['status'] = ($deposit->is_pending == 1) ? 'Processing' : 'Processed';
-          }
-
-          if($deposit->is_pending == 1){
-            $plan1['days_remaining'] = 'n/a';
-          }else{
-            $plan1['days_remaining'] = $this->calculate_remaining_days($plan1_days, $deposit->date_approved);
-          }
-
-          array_push($plan1_deposit_data, $plan1);
+        if ($payment_mode->name == 'XRP') {
+          $plan1['send_to'] = $payment_mode->tag . ' / ' . $payment_mode->account;
+        } else {
+          $plan1['send_to'] = $payment_mode->account;
         }
-        else if($deposit->package_id == 2){
-          $plan2 = array();
-          $plan2['amount'] = number_format($deposit->amount, 2, '.', ',');
 
-          $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
-          $plan2['mode'] = $payment_mode->name;
+        $plan1['date'] = $deposit->date;
+        $plan1['date_approved'] = $deposit->date_approved;
 
-          if($payment_mode->name == 'XRP'){
-            $plan2['send_to'] = $payment_mode->tag .' / '. $payment_mode->account;
-          }else{
-            $plan2['send_to'] = $payment_mode->account;
-          }
-          
-          $plan2['date'] = $deposit->date;
-          $plan2['date_approved'] = $deposit->date_approved;
-
-          if($deposit->deposit_options_id == 7){
-            $plan2['status'] = 'Processed';
-          }else{
-            $plan2['status'] = ($deposit->is_pending == 1) ? 'Processing' : 'Processed';
-          }
-
-          if($deposit->is_pending == 1){
-            $plan2['days_remaining'] = 'n/a';
-          }else{
-            $plan2['days_remaining'] = $this->calculate_remaining_days($plan2_days, $deposit->date_approved);
-          }
-
-          array_push($plan2_deposit_data, $plan2);
+        if ($deposit->deposit_options_id == 7) {
+          $plan1['status'] = 'Processed';
+        } else {
+          $plan1['status'] = ($deposit->is_pending == 1) ? 'Processing' : 'Processed';
         }
-        else if($deposit->package_id == 5){
-          $plan3 = array();
-          $plan3['amount'] = number_format($deposit->amount, 2, '.', ',');
 
-          $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
-          $plan3['mode'] = $payment_mode->name;
-
-          if($payment_mode->name == 'XRP'){
-            $plan3['send_to'] = $payment_mode->tag .' / '. $payment_mode->account;
-          }else{
-            $plan3['send_to'] = $payment_mode->account;
-          }
-          
-          $plan3['date'] = $deposit->date;
-          $plan3['date_approved'] = $deposit->date_approved;
-
-          if($deposit->deposit_options_id == 7){
-            $plan3['status'] = 'Processed';
-          }else{
-            $plan3['status'] = ($deposit->is_pending == 1) ? 'Processing' : 'Processed';
-          }
-
-          if($deposit->is_pending == 1){
-            $plan3['days_remaining'] = 'n/a';
-          }else{
-            $plan3['days_remaining'] = $this->calculate_remaining_days($plan3_days, $deposit->date_approved);
-          }
-
-          array_push($plan3_deposit_data, $plan3);
+        if ($deposit->is_pending == 1) {
+          $plan1['days_remaining'] = 'n/a';
+        } else {
+          $plan1['days_remaining'] = $this->calculate_remaining_days($plan1_days, $deposit->date_approved);
         }
-        else if($deposit->package_id == 6){
-          $plan4 = array();
-          $plan4['amount'] = number_format($deposit->amount, 2, '.', ',');
 
-          $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
-          $plan4['mode'] = $payment_mode->name;
+        array_push($plan1_deposit_data, $plan1);
+      } else if ($deposit->package_id == 2) {
+        $plan2 = array();
+        $plan2['amount'] = number_format($deposit->amount, 2, '.', ',');
 
-          if($payment_mode->name == 'XRP'){
-            $plan4['send_to'] = $payment_mode->tag .' / '. $payment_mode->account;
-          }else{
-            $plan4['send_to'] = $payment_mode->account;
-          }
-          
-          $plan4['date'] = $deposit->date;
-          $plan4['date_approved'] = $deposit->date_approved;
+        $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
+        $plan2['mode'] = $payment_mode->name;
 
-          if($deposit->deposit_options_id == 7){
-            $plan4['status'] = 'Processed';
-          }else{
-            $plan4['status'] = ($deposit->is_pending == 1) ? 'Processing' : 'Processed';
-          }
-
-          if($deposit->is_pending == 1){
-            $plan4['days_remaining'] = 'n/a';
-          }else{
-            $plan4['days_remaining'] = $this->calculate_remaining_days($plan4_days, $deposit->date_approved);
-          }
-
-          array_push($plan4_deposit_data, $plan4);
+        if ($payment_mode->name == 'XRP') {
+          $plan2['send_to'] = $payment_mode->tag . ' / ' . $payment_mode->account;
+        } else {
+          $plan2['send_to'] = $payment_mode->account;
         }
+
+        $plan2['date'] = $deposit->date;
+        $plan2['date_approved'] = $deposit->date_approved;
+
+        if ($deposit->deposit_options_id == 7) {
+          $plan2['status'] = 'Processed';
+        } else {
+          $plan2['status'] = ($deposit->is_pending == 1) ? 'Processing' : 'Processed';
+        }
+
+        if ($deposit->is_pending == 1) {
+          $plan2['days_remaining'] = 'n/a';
+        } else {
+          $plan2['days_remaining'] = $this->calculate_remaining_days($plan2_days, $deposit->date_approved);
+        }
+
+        array_push($plan2_deposit_data, $plan2);
+      } else if ($deposit->package_id == 5) {
+        $plan3 = array();
+        $plan3['amount'] = number_format($deposit->amount, 2, '.', ',');
+
+        $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
+        $plan3['mode'] = $payment_mode->name;
+
+        if ($payment_mode->name == 'XRP') {
+          $plan3['send_to'] = $payment_mode->tag . ' / ' . $payment_mode->account;
+        } else {
+          $plan3['send_to'] = $payment_mode->account;
+        }
+
+        $plan3['date'] = $deposit->date;
+        $plan3['date_approved'] = $deposit->date_approved;
+
+        if ($deposit->deposit_options_id == 7) {
+          $plan3['status'] = 'Processed';
+        } else {
+          $plan3['status'] = ($deposit->is_pending == 1) ? 'Processing' : 'Processed';
+        }
+
+        if ($deposit->is_pending == 1) {
+          $plan3['days_remaining'] = 'n/a';
+        } else {
+          $plan3['days_remaining'] = $this->calculate_remaining_days($plan3_days, $deposit->date_approved);
+        }
+
+        array_push($plan3_deposit_data, $plan3);
+      } else if ($deposit->package_id == 6) {
+        $plan4 = array();
+        $plan4['amount'] = number_format($deposit->amount, 2, '.', ',');
+
+        $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
+        $plan4['mode'] = $payment_mode->name;
+
+        if ($payment_mode->name == 'XRP') {
+          $plan4['send_to'] = $payment_mode->tag . ' / ' . $payment_mode->account;
+        } else {
+          $plan4['send_to'] = $payment_mode->account;
+        }
+
+        $plan4['date'] = $deposit->date;
+        $plan4['date_approved'] = $deposit->date_approved;
+
+        if ($deposit->deposit_options_id == 7) {
+          $plan4['status'] = 'Processed';
+        } else {
+          $plan4['status'] = ($deposit->is_pending == 1) ? 'Processing' : 'Processed';
+        }
+
+        if ($deposit->is_pending == 1) {
+          $plan4['days_remaining'] = 'n/a';
+        } else {
+          $plan4['days_remaining'] = $this->calculate_remaining_days($plan4_days, $deposit->date_approved);
+        }
+
+        array_push($plan4_deposit_data, $plan4);
+      }
     }
 
     $data['plan1_deposit_data'] = $plan1_deposit_data;
@@ -180,16 +177,17 @@ class Your_deposits extends CI_Controller
     // $this->load->view('templates/footer');
   }
 
-  public function calculate_remaining_days($contract_days, $date_approved){
+  public function calculate_remaining_days($contract_days, $date_approved)
+  {
     $start_date = new DateTime($date_approved);
     $end_date = DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s'));
 
     // print_r($start_date);
     // print_r($end_date);
 
-    
+
     $days  = $end_date->diff($start_date)->format('%a');
-    
+
     // print_r($days);
 
     // $difference = $start_date->diff($end_date);
@@ -197,5 +195,4 @@ class Your_deposits extends CI_Controller
 
     return abs($remaining_days);
   }
-
 }
