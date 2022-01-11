42ec1f5061cfa95accab7eeaa9050fad65d08334
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2022-01-11 21:01:29 +0800

    announcement

--- application/views/login/registration.php
+++ application/views/login/registration.php
@@ -7,7 +7,7 @@
     <title><?php echo $title; ?> &mdash; Equifinance</title>
 
     <!-- favicon -->
-    <link rel="icon" href="<?=base_url()?>assets/img/favicon.ico" type="image/gif">
+    <link rel="icon" href="<?= base_url() ?>assets/img/favicon.ico" type="image/gif">
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/bootstrap/css/bootstrap.min.css">
@@ -23,15 +23,15 @@
 
     <!-- CSS Libraries -->
     <?php
-  if ($this->uri->segment(2) == "" || $this->uri->segment(2) == "index") {
-      ?>
+    if ($this->uri->segment(2) == "" || $this->uri->segment(2) == "index") {
+    ?>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/jqvmap/dist/jqvmap.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/summernote/summernote-bs4.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/owlcarousel2/dist/assets/owl.carousel.min.css">
     <link rel="stylesheet"
         href="<?php echo base_url(); ?>assets/modules/owlcarousel2/dist/assets/owl.theme.default.min.css">
     <?php
-  } ?>
+    } ?>
 
     <!-- Template CSS -->
 
@@ -89,8 +89,8 @@
                         <div class="row">
                             <div class="col-12 text-center">
                                 <div class="login-brand">
-                                    <a href="<?= base_url();?>">
-                                        <img src="<?= base_url();?>assets/img/EF_LOGO_COMPLETE.svg" alt="logo"
+                                    <a href="<?= base_url(); ?>">
+                                        <img src="<?= base_url(); ?>assets/img/EF_LOGO_COMPLETE.svg" alt="logo"
                                             width="300">
                                     </a>
                                 </div>
@@ -103,7 +103,7 @@
                                     <h3 class="reg-title">REGISTRATION</h3>
 
                                     <!-- <div class="reg_title">
-                <p>Already have an account? <a href="<?= base_url();?>login">Login</a></p>
+                <p>Already have an account? <a href="<?= base_url(); ?>login">Login</a></p>
                 <br>
             </div> -->
 
@@ -111,7 +111,7 @@
                             </div>
 
                             <!-- <div class="reg_title">
-    <p>Already have an account? <a href="<?= base_url();?>login">Login</a></p>
+    <p>Already have an account? <a href="<?= base_url(); ?>login">Login</a></p>
     <br>
 </div> -->
 
@@ -125,11 +125,13 @@
                                 <label for="username" class="log_reg_label">Username:
                                 </label>
                                 <input id="reg-username logreg_input" type="text"
-                                    value="<?= set_value('username','',true)?>"
-                                    class="form-control login_input <?php if(strlen(form_error('username')) > 0){echo "is-invalid";} ?>"
+                                    value="<?= set_value('username', '', true) ?>"
+                                    class="form-control login_input <?php if (strlen(form_error('username')) > 0) {
+                                                                                                                                                                        echo "is-invalid";
+                                                                                                                                                                    } ?>"
                                     name="username" autofocus required />
                                 <div class="invalid-feedback">
-                                    <?php echo form_error('username');?>
+                                    <?php echo form_error('username'); ?>
                                 </div>
                             </div>
                             <!-- </div>
@@ -138,11 +140,13 @@
                                 <label for="fullname" class="log_reg_label">Fullname:
                                 </label>
                                 <input id="fullname logreg_input" type="text"
-                                    value="<?= set_value('fullname','',true)?>"
-                                    class="form-control login_input <?php if(strlen(form_error('fullname')) > 0){echo "is-invalid";} ?>"
+                                    value="<?= set_value('fullname', '', true) ?>"
+                                    class="form-control login_input <?php if (strlen(form_error('fullname')) > 0) {
+                                                                                                                                                                    echo "is-invalid";
+                                                                                                                                                                } ?>"
                                     name="fullname" autofocus required />
                                 <div class="invalid-feedback">
-                                    <?php echo form_error('fullname');?>
+                                    <?php echo form_error('fullname'); ?>
                                 </div>
                             </div>
                             <!-- </div>
@@ -152,10 +156,11 @@
                                 <label for="password" class="log_reg_label">Create Password:
                                 </label>
                                 <input id="reg-password logreg_input" type="password"
-                                    class="form-control login_input <?php if(strlen(form_error('password')) > 0){echo "is-invalid";} ?>"
-                                    name="password" autofocus required />
+                                    class="form-control login_input <?php if (strlen(form_error('password')) > 0) {
+                                                                                                                            echo "is-invalid";
+                                                                                                                        } ?>" name="password" autofocus required />
                                 <div class="invalid-feedback">
-                                    <?php echo form_error('password');?>
+                                    <?php echo form_error('password'); ?>
                                 </div>
                             </div>
                             <!-- </div>
@@ -168,11 +173,13 @@
                                 <label for="confirm_password" class="log_reg_label">Repeat Password:
                                 </label>
                                 <input id="confirm_password logreg_input" type="password"
-                                    value="<?= set_value('confirm_password','',true)?>"
-                                    class="form-control login_input <?php if(strlen(form_error('confirm_password')) > 0){echo "is-invalid";} ?>"
+                                    value="<?= set_value('confirm_password', '', true) ?>"
+                                    class="form-control login_input <?php if (strlen(form_error('confirm_password')) > 0) {
+                                                                                                                                                                                        echo "is-invalid";
+                                                                                                                                                                                    } ?>"
                                     name="confirm_password" autofocus required />
                                 <div class="invalid-feedback">
-                                    <?php echo form_error('confirm_password');?>
+                                    <?php echo form_error('confirm_password'); ?>
                                 </div>
                             </div>
                             <!-- </div>
@@ -180,11 +187,13 @@
                             <div class="form-group">
                                 <label for="email" class="log_reg_label">Email Address:
                                 </label>
-                                <input id="email logreg_input" type="text" value="<?= set_value('email','',true)?>"
-                                    class="form-control login_input <?php if(strlen(form_error('email')) > 0){echo "is-invalid";} ?>"
+                                <input id="email logreg_input" type="text" value="<?= set_value('email', '', true) ?>"
+                                    class="form-control login_input <?php if (strlen(form_error('email')) > 0) {
+                                                                                                                                                            echo "is-invalid";
+                                                                                                                                                        } ?>"
                                     name="email" autofocus required />
                                 <div class="invalid-feedback">
-                                    <?php echo form_error('email');?>
+                                    <?php echo form_error('email'); ?>
                                 </div>
                             </div>
                             <!-- </div>
@@ -194,10 +203,11 @@
                                 <label for="confirm_email" class="log_reg_label">Repeat Email:
                                 </label>
                                 <input id="confirm_email logreg_input" type="text"
-                                    class="form-control login_input <?php if(strlen(form_error('confirm_email')) > 0){echo "is-invalid";} ?>"
-                                    name="confirm_email" autofocus required />
+                                    class="form-control login_input <?php if (strlen(form_error('confirm_email')) > 0) {
+                                                                                                                        echo "is-invalid";
+                                                                                                                    } ?>" name="confirm_email" autofocus required />
                                 <div class="invalid-feedback">
-                                    <?php echo form_error('confirm_email');?>
+                                    <?php echo form_error('confirm_email'); ?>
                                 </div>
                             </div>
                             <!-- </div>
@@ -210,11 +220,13 @@
                                 <label for="referral" class="log_reg_label">Referred By:
                                 </label>
                                 <input id="referral logreg_input" type="text"
-                                    value="<?= set_value('referral',$referral_code,true)?>"
-                                    class="form-control login_input <?php if(strlen(form_error('referral')) > 0){echo "is-invalid";} ?>"
+                                    value="<?= set_value('referral', $referral_code, true) ?>"
+                                    class="form-control login_input <?php if (strlen(form_error('referral')) > 0) {
+                                                                                                                                                                                echo "is-invalid";
+                                                                                                                                                                            } ?>"
                                     name="referral" autofocus required />
                                 <div class="invalid-feedback">
-                                    <?php echo form_error('referral');?>
+                                    <?php echo form_error('referral'); ?>
                                 </div>
                             </div>
                             <!-- </div> -->
@@ -230,7 +242,12 @@
                                         Account</button>
                                 </div>
 
-                                <!-- <div class="col-sm-3"></div> -->
+                                <div class="col-sm-12 text-center text-white">
+                                    <span class="login-link">
+                                        Already a member?
+                                        <a href="<?= base_url('login'); ?>" class="text-danger">Login</a>
+                                    </span>
+                                </div>
                             </div>
 
                             </form>
@@ -253,6 +270,6 @@
 
 <!-- <?php $this->load->view('dist/_partials/js'); ?> -->
 <script src="https://kit.fontawesome.com/yourcode.js"></script>
-<script src="<?php echo base_url();?>assets/js/countrypicker.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/countrypicker.js"></script>
 
 </html>
