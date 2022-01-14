2f452b3c230ec35a97f6599125f1bdf46e7b0ded
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2022-01-14 21:09:34 +0800

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,27 +3,27 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Login  extends CI_Controller
 {
-		public function __construct()
-        {
-            parent::__construct();
-						$this->load->model('members');
-						$this->load->model('Referral_codes');
-        }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('members');
+		$this->load->model('Referral_codes');
+	}
 
 	public function index()
 	{
 		$this->members->update_v_code();
 
-		if(isset($this->session->username)){
+		if (isset($this->session->username)) {
 
-			if($this->session->is_admin){
+			if ($this->session->is_admin) {
 
 				redirect('deposits_admin', 'refresh');
-			}else{
+			} else {
 
 				redirect('dashboard', 'refresh');
 			}
-		}else{
+		} else {
 			$data = array(
 				'title' => "Login"
 			);
@@ -35,10 +35,9 @@ class Login  extends CI_Controller
 				// $this->load->view('login/header', $data);
 				$this->load->view('login/login', $data);
 				// $this->load->view('login/footer');
-			}
-			else {
+			} else {
 
-				if($this->is_login_valid()){
+				if ($this->is_login_valid()) {
 
 					// printf($this->members->get_member($_POST['email']));
 					$member_data = $this->members->get_member($_POST['username']);
@@ -58,32 +57,33 @@ class Login  extends CI_Controller
 						'last_access' => $member_data->last_access
 					);
 
-					if($member_data->account_type_id == '1' || $member_data->account_type_id == '3'){
+					if ($member_data->account_type_id == '1' || $member_data->account_type_id == '3') {
 						$userdata['is_admin'] = true;
-					}else{
+					} else {
 						$userdata['is_admin'] = false;
 					}
 
-					
+
 
 					$this->load->model('DepositModel');
 
-					if($member_data->account_type_id == '1' || $member_data->account_type_id == '3'){
+					if ($member_data->account_type_id == '1' || $member_data->account_type_id == '3') {
 						$this->session->set_userdata($userdata);
 						redirect('deposits_admin');
-					}else{
+					} else {
 						$auth_code = random_string('numeric', 5);
 						$is_code_updated =  $this->members->update_auth($member_data->id, $auth_code);
-						if($is_code_updated == 1){
-							$is_email_sent = $this->send_auth_code($member_data->username, $auth_code, $member_data->email_address);
-							if($is_email_sent){
+						if ($is_code_updated == 1) {
+							// $is_email_sent = $this->send_auth_code($member_data->username, $auth_code, $member_data->email_address);
+							$is_email_sent = false;
+							if ($is_email_sent) {
 								$user['user_id'] = $member_data->id;
 								$this->session->set_userdata($user);
 
 								$this->session->set_flashdata('member_id', $member_data->id);
 								redirect('authorize');
 							}
-						}else{
+						} else {
 							redirect('login');
 						}
 
@@ -93,51 +93,49 @@ class Login  extends CI_Controller
 						// 	redirect('plans');
 						// }
 					}
-
-				}else{
+				} else {
 					// $this->load->view('login/header', $data);
 					$this->load->view('login/login', $data);
 					// $this->load->view('login/footer');
 
 				}
 			}
-
 		}
-
 	}
 
-	function is_login_valid(){
+	function is_login_valid()
+	{
 
-		$is_valid = $this->members->verify_member($_POST['username'],$_POST['password']);
+		$is_valid = $this->members->verify_member($_POST['username'], $_POST['password']);
 
-		if($is_valid){
+		if ($is_valid) {
 			return true;
-		}else{
-			$this->form_validation->set_message('is_login_valid','Invalid username or password.');
+		} else {
+			$this->form_validation->set_message('is_login_valid', 'Invalid username or password.');
 			return false;
 		}
 	}
 
-	function send_auth_code($username, $code, $email){
+	function send_auth_code($username, $code, $email)
+	{
 
-		ini_set( 'display_errors', 1 );
-        error_reporting( E_ALL );
+		ini_set('display_errors', 1);
+		error_reporting(E_ALL);
 
 		$this->load->library('email');
 
 		$userdata['member_username'] = $username;
 		$userdata['auth_code'] = $code;
 
-        $this->email->from('support@member-equifinance.com', 'Equifinance')
-            ->to($email)
-            ->subject('Login Authentication')
-						->message($this->load->view('email/authentication', $userdata, true));
+		$this->email->from('support@member-equifinance.com', 'Equifinance')
+			->to($email)
+			->subject('Login Authentication')
+			->message($this->load->view('email/authentication', $userdata, true));
 
-        if($this->email->send()){
-					return true;
-				}else{
-					return false;
-				}
+		if ($this->email->send()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-
 }
